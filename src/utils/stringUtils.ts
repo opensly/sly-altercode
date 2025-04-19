32f980783bece838d06e2d5ecedde15fe783b572
Lakshmikanth Vallampati <kanth.vallampati@gmail.com>
2025-04-19 12:33:31 +0530

    Evolving from stringUtils.js to stringUtils.ts

--- src/utils/stringUtils.ts
+++ src/utils/stringUtils.ts
@@ -1,6 +1,6 @@
 /**
  * Escapes special characters in a string for use in a regular expression
  */
-export function escapeRegExp(string) {
+export function escapeRegExp(string: string): string {
   return string.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
 }
